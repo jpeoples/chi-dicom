a4ba4fb80d61f3d0de66e16da2f9fafe89b491b1
Jacob Peoples <jacobjpeoples@gmail.com>
2024-06-18 06:47:07 -0400

    Add parallel processing and batch prep

--- chi/dcmscanner.py
+++ chi/dcmscanner.py
@@ -30,6 +30,29 @@ import pandas
 import pydicom
 from tqdm import tqdm
 
+import time
+
+from joblib import Parallel, delayed
+
+
+class Tic:
+    def __init__(self):
+        self.tic()
+
+    def get_time(self):
+        return time.perf_counter_ns()
+
+    def process_diff(self, diff):
+        return diff / 1e9
+
+    def tic(self):
+        self._last = self.get_time()
+    
+    def toc(self):
+        diff = self.get_time() - self._last
+        return self.process_diff(diff)
+
+
 def make_parser(f=None):
     parser = argparse.ArgumentParser()
     if f:
@@ -87,7 +110,10 @@ class EntryPoints:
 
     def main(self):
         args = self.parse_args()
+        tic = Tic()
         args.cmd(args)
+        tdiff = tic.toc()
+        print(f"Ran in {tdiff:0.05f} seconds")
 
 def main():
     entry.main()
@@ -191,6 +217,21 @@ def get_index_file_pairs_for_args(args, index=None):
                 with zf.open(name) as fp:
                     yield ix, fp
 
+def scan_process_zip(zfname, tab, args, name_mapping, tag_set):
+    def fix_val(x):
+        return "" if x is None else str(x.value)
+    tag_to_string = lambda t: name_mapping.get(t, t.tag_string())
+
+    zfpath = os.path.join(args.root, zfname)
+    read_results = {}
+    with zipfile.ZipFile(zfpath, "r") as zf:
+        for ix, name in tab['ArcName'].items():
+            with zf.open(name) as fp:
+                with pydicom.dcmread(fp, stop_before_pixels=True, specific_tags=tag_set) as dcm:
+                    read_results[ix] = {tag_to_string(tag): fix_val(dcm.get(tag)) for tag in tag_set} 
+
+    return read_results
+
 def make_empty_df(index_cols, col_names):
     if len(index_cols) > 1:
         ix = pandas.MultiIndex.from_arrays([[]]*len(index_cols), names=index_cols)
@@ -213,6 +254,15 @@ def load_index(args):
     index = pandas.read_csv(args.index, index_col=0)
     return index
 
+def reduce_table_for_batch(index, args):
+    if args.batch_size > 0 and args.batch_offset > -1:
+        zips = sorted(index['ZipFile'].unique())
+        zips = set(zips[args.batch_offset:args.batch_offset+args.batch_size])
+        
+        index = index.loc[index['ZipFile'].isin(zips)]
+
+    return index
+
 @entry.point
 def scan(args):
     tag_set, name_mapping = get_tag_set_for_args(args)
@@ -220,16 +270,17 @@ def scan(args):
 
     read_results = {}
 
-    def fix_val(x):
-        return "" if x is None else str(x.value)
-    tag_to_string = lambda t: name_mapping.get(t, t.tag_string())
 
     index = load_index(args)
-    for ix, fp in get_index_file_pairs_for_args(args, index):
-        with pydicom.dcmread(fp, stop_before_pixels=True, specific_tags=tag_set) as dcm:
-            read_results[ix] = {tag_to_string(tag): fix_val(dcm.get(tag)) for tag in tag_set} 
+    index = reduce_table_for_batch(index, args)
+
+    results = Parallel(n_jobs=args.jobs, verbose=10)(delayed(scan_process_zip)(zfname, tab, args, name_mapping, tag_set) for zfname, tab in index.groupby("ZipFile"))
+    for res in results:
+        read_results.update(res)
 
 
+
+    tag_to_string = lambda t: name_mapping.get(t, t.tag_string())
     tag_labels = [tag_to_string(t) for t in tag_set]
     scan_results = make_df_from_result_dict(read_results, args, tag_labels)
 
@@ -243,10 +294,28 @@ def scan_parser(parser):
     parser.add_argument("--tags", nargs="+", required=True, action='extend')
     parser.add_argument("--index", required=True)
     parser.add_argument("--tag_conf", required=False)
+    parser.add_argument("--batch_offset", required=False, default=-1, type=int)
+    parser.add_argument("--batch_size", required=False, default=0, type=int)
 
 def fix_path(path):
     return pathlib.Path(path).as_posix()
 
+def zip_archive_index_process(z, relz, args):
+    relz = fix_path(os.path.relpath(z, args.root))
+
+    zdir = os.path.dirname(relz)
+    try:
+        zf = zipfile.ZipFile(z, "r")
+    except zipfile.BadZipFile:
+        print("Bad zip:", z)
+        return None
+    
+    with zf:
+        names = zf.namelist()
+        full_index = [fix_path(os.path.join(zdir, name)) for name in names]
+        index = pandas.Index(full_index, name="FileName")
+        df = pandas.DataFrame({"ZipFile": [relz]*len(index), "ArcName": names}, index=index)
+        return df
 @entry.point
 def zip_archive_index(args):
     zips = dicom.list_files(args.root, "*.zip")
@@ -260,26 +329,31 @@ def zip_archive_index(args):
         tables = []
     
     
-    relzips = [os.path.relpath(z, args.root) for z in zips]
+    relzips = [fix_path(os.path.relpath(z, args.root)) for z in zips]
 
     zrelz = [(z, relz) for z, relz in zip(zips, relzips) if relz not in existing_zips]
+    print(len(relzips), len(zips), len(zrelz))
 
-    for z, relz in tqdm(zrelz):
-        relz = os.path.relpath(z, args.root)
 
-        zdir = os.path.dirname(relz)
-        try:
-            zf = zipfile.ZipFile(z, "r")
-        except zipfile.BadZipFile:
-            print("Bad zip:", z)
-            continue
-        
-        with zf:
-            names = zf.namelist()
-            full_index = [os.path.join(zdir, name) for name in names]
-            index = pandas.Index(full_index, name="FileName")
-            df = pandas.DataFrame({"ZipFile": [relz]*len(index), "ArcName": names}, index=index)
-            tables.append(df)
+    new_tables = Parallel(n_jobs=args.jobs, verbose=10)(delayed(zip_archive_index_process)(z, relz, args) for z, relz in zrelz)
+    tables.extend([t for t in new_tables if t is not None])
+
+    #for z, relz in tqdm(zrelz):
+    #    relz = fix_path(os.path.relpath(z, args.root))
+
+    #    zdir = os.path.dirname(relz)
+    #    try:
+    #        zf = zipfile.ZipFile(z, "r")
+    #    except zipfile.BadZipFile:
+    #        print("Bad zip:", z)
+    #        continue
+    #    
+    #    with zf:
+    #        names = zf.namelist()
+    #        full_index = [fix_path(os.path.join(zdir, name)) for name in names]
+    #        index = pandas.Index(full_index, name="FileName")
+    #        df = pandas.DataFrame({"ZipFile": [relz]*len(index), "ArcName": names}, index=index)
+    #        tables.append(df)
     
     outdf = pandas.concat(tables, axis='rows')
     outdf.to_csv(args.output)
@@ -290,7 +364,36 @@ def zip_archive_index_parser(parser):
     parser.add_argument("--output", required=True)
     parser.add_argument("--reread_all", action='store_true')
 
+@entry.point
+def index_info(args):
+    index = load_index(args)
+    zipfiles = sorted(index['ZipFile'].unique())
 
+    nzips = len(zipfiles)
+    print("There are ", nzips, "zip files")
+
+    if args.batch_size > 0:
+        mod = nzips % args.batch_size
+        if mod == 0:
+            max_base = nzips - args.batch_size
+        else:
+            max_base = nzips - mod
+
+        batches = max_base / args.batch_size
+        print("With batch size", args.batch_size, "we will use", batches, "tasks")
+        print(f"0-{max_base}:{args.batch_size}")
+
+        
+
+
+@index_info.parser
+def index_info_parser(parser):
+    parser.add_argument("--index", required=True)
+    parser.add_argument("--batch_size", required=False, type=int, default=0)
+
+@entry.add_common_parser
+def common_parser(parser):
+    parser.add_argument("--jobs", type=int, default=1)
 
 
 
